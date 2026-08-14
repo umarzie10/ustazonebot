3d093e843e28abaa3d8d7aa1b34ab485266720db
umare <umareshkurbonov@gmail.com>
2026-08-14 16:13:41 +0500

    Fix import errors in keyboards init

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,19 +1,21 @@
 from .menu import (
     get_main_menu, 
     get_socials_menu, 
-    get_faq_menu, 
-    get_faq_back_menu, 
+    get_yordam_menu, 
+    get_yordam_back_menu, 
     get_news_pagination,
     get_about_menu,
-    get_admin_menu
+    get_admin_menu,
+    get_contact_operator_menu
 )
 
 __all__ = [
     "get_main_menu",
     "get_socials_menu",
-    "get_faq_menu",
-    "get_faq_back_menu",
+    "get_yordam_menu",
+    "get_yordam_back_menu",
     "get_news_pagination",
     "get_about_menu",
-    "get_admin_menu"
+    "get_admin_menu",
+    "get_contact_operator_menu"
 ]
